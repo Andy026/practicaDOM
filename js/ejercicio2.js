031c2186a4c2df87bb2c538a65ae3fc6c63e9b8a
Andy <andysatie@gmail.com>
2021-09-30 00:30:04 -0300

    30/9 js dom y bom

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -11,29 +11,30 @@ class Persona {
   //metodos
   mostrarGeneracion() {
     if (this.anioNacimiento > 1930 && this.anioNacimiento < 1948) {
-      return document.write(`Perteneces a la Generación Silenciosa.
+      return alert(`Perteneces a la Generación Silenciosa.
                         <br>Tu rasgo caracteristico es la austeridad.`);
     } else if (this.anioNacimiento > 1949 && this.anioNacimiento < 1968) {
-      return document.write(`Perteneces a la Generación Baby Boom.
+      return alert(`Perteneces a la Generación Baby Boom.
                         <br>Tu rasgo caracteristico es la ambición.`);
     } else if (this.anioNacimiento > 1969 && this.anioNacimiento < 1980) {
-      return document.write(`Perteneces a la Generación X.
+      return alert(`Perteneces a la Generación X.
                         <br>Tu rasgo caracteristico es la obsesión por el éxito.`);
     } else if (this.anioNacimiento > 1981 && this.anioNacimiento < 1993) {
-      return document.write(`Perteneces a la Generación Y (Millennials).
+      return alert(`Perteneces a la Generación Y (Millennials).
                         <br>Tu rasgo caracteristico es la frustración.`);
     } else if (this.anioNacimiento > 1994 && this.anioNacimiento < 2010) {
-      return document.write(`Perteneces a la Generación Z
+      return alert(`Perteneces a la Generación Z
                         <br>Tu rasgo caracteristico es la irreverencia.`);
     }
   }
 
   esMayordeEdad() {
-    if (this.edad >= 18) return document.write("Eres mayor de edad.");
+    if (this.edad >= 18) return alert("Eres mayor de edad.");
   }
 
-  mostrarDatos() {
-    document.write(`
+  mostrarDatos(input) {
+    console.log(input.value);
+    alert(`
         Nombre: ${this.nombre}
         <br>Edad: ${this.edad}
         <br>DNI: ${this.dni}
@@ -58,6 +59,7 @@ function validarCampoRequerido(input) {
 }
 
 function validarNumeros(input) {
+  console.log(input.value);
   //validar con expresiones regulares
   let patron = /^[0-9]{1,8}$/;
   if (patron.test(input.value)) {
@@ -74,16 +76,30 @@ let sexo = document.querySelector("#sexo");
 let peso = document.querySelector("#peso");
 let altura = document.querySelector("#altura");
 let anioNacimiento = document.querySelector("#anioNacimiento");
+let mayordeEdad = document.querySelector('#mayordeEdad');
+let mostrarGen = document.querySelector('#mostrarGen');
+
+mostrarGen.addEventListener('submit', () => {mostrarGeneracion});
+mayordeEdad.addEventListener('submit', () => {esMayordeEdad});
 
 nombre.addEventListener("blur", () => {
   validarCampoRequerido(nombre);
 });
-edad.addEventListener('blur', ()=>{validarNumeros(edad)});
-dni.addEventListener('blur', ()=>{validarNumeros(dni)});
-sexo.addEventListener('blur', ()=>{validarCampoRequerido(sexo)});
-peso.addEventListener('blur', ()=>{validarNumeros(peso)});
-altura.addEventListener('blur', ()=>{validarNumeros(altura)});
-anioNacimiento.addEventListener('blur', ()=>{validarNumeros(anioNacimiento)});
-
-
-
+edad.addEventListener("blur", () => {
+  validarNumeros(edad);
+});
+dni.addEventListener("blur", () => {
+  validarNumeros(dni);
+});
+sexo.addEventListener("blur", () => {
+  validarCampoRequerido(sexo);
+});
+peso.addEventListener("blur", () => {
+  validarNumeros(peso);
+});
+altura.addEventListener("blur", () => {
+  validarNumeros(altura);
+});
+anioNacimiento.addEventListener("blur", () => {
+  validarNumeros(anioNacimiento);
+});
